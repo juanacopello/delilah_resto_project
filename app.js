6def8711e73d916f7684a8322e7613b451422bbc
Juana Copello <61588695+juanacopello@users.noreply.github.com>
2020-09-20 22:43:19 -0300

    MySql

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require ('express');
 const server = express();
+const bodyParser = require('body-parser');
+server.use(bodyParser.json())
+const sequelize = require('./sql')
 
 server.listen(3000, () => console.log("servidor iniciado"))
+
+
+
+//PUT actualizar estado de pedido
+//POST crear pedido
+//VALIDACION para ingresar a la plataforma
---
+++ sql.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:@localhost:/delilah_resto')
+
+module.exports = sequelize
