b2aeb2a04c06299b6ba9a57cadc0e8167618e93b
Juana Copello <61588695+juanacopello@users.noreply.github.com>
2020-09-21 21:15:42 -0300

    sequelize

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const sequelize = require('./sql')
 
 server.listen(3000, () => console.log("servidor iniciado"))
 
+//Get Users from Users Database
+
+server.get('/users', async (req, res) => {
+  const data = await sequelize.query(
+    'SELECT * FROM users WHERE role_id = 2',
+    {type: sequelize.QueryTypes.SELECT},
+  )
+  res.send(data)
+  console.log(data)
+})
+
+//Get Administrators from Users Database
+
+
+server.get('/users/:role_id', async (req, res) => {
+  
+  const data = await sequelize.query(
+    'SELECT * FROM users WHERE role_id = 1',
+    {type: sequelize.QueryTypes.SELECT},
+  )
+  res.send(data)
+})
 
 
 //PUT actualizar estado de pedido
